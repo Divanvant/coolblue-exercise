428bef132538c870d2c98e5aaf8f1cc62c5bb8d9
nonadivan <divan@nona.digital>
2023-01-03 16:52:15 +0200

    refactor: neaten up javascript code and fix add to cart modal close bug

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,27 @@
-const removeAllActiveStates = (accordionItems) => {
-  accordionItems.forEach((item) => {
+const removeAllActiveStates = (items) => {
+  items.forEach((item) => {
     item.classList.remove('active')
   })
 }
 
+document
+  .querySelector('#more-information-modal')
+  .addEventListener('click', () => {
+    document.querySelector('#more-information-modal').classList.remove('active')
+  })
+
 document.addEventListener('DOMContentLoaded', () => {
   const accordionItems = document.querySelectorAll('.accordion-item')
+  const modalWrappers = document.querySelectorAll('.modal-wrapper')
+
+  const moreInfoModal = document.querySelector('#more-information-modal')
+  const moreInfoButton = document.querySelector('#more-information-button')
+  const moreInformationClose = document.querySelector('#more-information-close')
+
+  const getStartedModal = document.querySelector('#get-started-modal')
+  const getStartedButton = document.querySelector('#get-started-button')
+  const getStartedClose = document.querySelector('#get-started-close')
+
   accordionItems.forEach((item) => {
     item.addEventListener('click', (event) => {
       const currentAccordionItem = event.target.closest('.accordion-item')
@@ -18,36 +34,36 @@ document.addEventListener('DOMContentLoaded', () => {
     })
   })
 
-  document
-    .querySelector('#more-information-button')
-    .addEventListener('click', () => {
-      document.querySelector('#more-information-modal').classList.add('active')
+  modalWrappers.forEach((item) => {
+    item.addEventListener('click', () => {
+      removeAllActiveStates(modalWrappers)
     })
-
-  document
-    .querySelector('#more-information-close')
-    .addEventListener('click', () => {
-      document
-        .querySelector('#more-information-modal')
-        .classList.remove('active')
-    })
-
-  document
-    .querySelector('#get-started-button')
-    .addEventListener('click', () => {
-      document.querySelector('#get-started-modal').classList.add('active')
-    })
-
-  document.querySelector('#get-started-close').addEventListener('click', () => {
-    document.querySelector('#get-started-modal').classList.remove('active')
   })
 
-  document.querySelector('#add-to-cart-basic').addEventListener('click', () => {
-    alert('Added Warning Red Alert Basic to cart')
+  moreInfoButton.addEventListener('click', () => {
+    moreInfoModal.classList.add('active')
   })
+  moreInformationClose.addEventListener('click', () => {
+    moreInfoModal.classList.remove('active')
+  })
+
+  getStartedButton.addEventListener('click', () => {
+    getStartedModal.classList.add('active')
+  })
+  getStartedClose.addEventListener('click', () => {
+    getStartedModal.classList.remove('active')
+  })
+
+  document
+    .querySelector('#add-to-cart-basic')
+    .addEventListener('click', (event) => {
+      event.stopPropagation()
+      alert('Added Warning Red Alert Basic to cart')
+    })
   document
     .querySelector('#add-to-cart-premium')
-    .addEventListener('click', () => {
+    .addEventListener('click', (event) => {
+      event.stopPropagation()
       alert('Added Warning Red Alert Premium to cart')
     })
 })
