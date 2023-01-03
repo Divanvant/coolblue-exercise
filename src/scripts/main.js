71e5940b0daad625ea8b8047aad941215d54c517
nonadivan <divan@nona.digital>
2023-01-03 15:24:56 +0200

    feat: add accordion function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,20 @@
+const removeAllActiveStates = (accordionItems) => {
+  accordionItems.forEach((item) => {
+    item.classList.remove('active')
+  })
+}
+
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('js is processed');
+  const accordionItems = document.querySelectorAll('.accordion-item')
+  accordionItems.forEach((item) => {
+    item.addEventListener('click', (event) => {
+      const currentAccordionItem = event.target.closest('.accordion-item')
+      if (currentAccordionItem.classList.contains('active')) {
+        removeAllActiveStates(accordionItems)
+      } else {
+        removeAllActiveStates(accordionItems)
+        currentAccordionItem.classList.add('active')
+      }
+    })
+  })
 })
